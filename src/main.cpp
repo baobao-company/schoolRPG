43eb1c6b0ea991d3d98472163225f11b4a6795c1
jiushi <2814925480@qq.com>
2026-07-17 10:07:19 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
-#include "GameManager.h"
-#include <iostream>
-#include <cstdlib>
-
-//主函数
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <SFML/Graphics.hpp>
+#include "src/core/GameManager.h"
 int main()
- {
-    // 设置控制台编码（Windows）
-    #ifdef _WIN32
-        system("chcp 65001 > nul");
-    #endif
-    // 创建游戏管理器
+{
+#ifdef _WIN32
+    SetConsoleOutputCP(65001);
+#endif
+    sf::RenderWindow window(sf::VideoMode(1280, 720), L"校园RPG");
+    window.setFramerateLimit(60);
+    sf::Font globalFont;
+    std::string fontPath = "res/fonts/msyh.ttc";
+    if (!globalFont.loadFromFile(fontPath))
+    {
+        MessageBoxA(nullptr, "字体文件缺失 res/fonts/msyh.ttc", "资源错误", MB_ICONERROR);
+        return -1;
+    }
     GameManager game;
-    // 运行游戏
-    game.run();
+    sf::Event event;
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+           if (event.type == sf::Event::Closed)
+            {
+                game.doSave();
+                window.close();
+            }
+            game.handleSFEvent(event, window);
+        }
+        window.clear(sf::Color(15,15,25));
+        game.renderAll(window, globalFont);
+        window.display();
+    }
     return 0;
 }
